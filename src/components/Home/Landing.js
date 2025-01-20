1162658084193fe20aae56f49e07bc1a7a66eec5
FanmartIndia <fanmartindia@gmail.com>
2025-01-20 19:46:36 +0530

    Old backend url added

--- src/components/Home/Landing.js
+++ src/components/Home/Landing.js
@@ -74,7 +74,7 @@ const Landing = () => {
       if (customUrlEnabled) {
         try {
           const { data } = await axios.get(
-            `https://vmxwrkamih.execute-api.ap-south-1.amazonaws.com/api/check-custom/${values?.customUrlPart}`
+            `https://birthday-cake-backend-1.onrender.com/api/check-custom/${values?.customUrlPart}`
           );
           if (data.exists) {
             toast.error("Custom URL already taken. Please choose another.", {
@@ -93,7 +93,7 @@ const Landing = () => {
 
             try {
               const { data } = await axios.post(
-                "https://vmxwrkamih.execute-api.ap-south-1.amazonaws.com/api/payment",
+                "https://birthday-cake-backend-1.onrender.com/api/payment",
                 payData
               );
               if (data?.redirectUrl) {
@@ -123,7 +123,7 @@ const Landing = () => {
 
         try {
           const response = await axios.post(
-            "https://vmxwrkamih.execute-api.ap-south-1.amazonaws.com/api/create-user",
+            "https://birthday-cake-backend-1.onrender.com/api/create-user",
             SendData
           );
 
